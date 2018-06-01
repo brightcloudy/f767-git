d71c48a0bd983cbdc8bc5f6247832146ce068ce5
Ryan Karl <brightycloud@gmail.com>
2018-06-01 14:39:30 -0400

    Modify thing...

--- main.cpp
+++ main.cpp
@@ -2,11 +2,29 @@
 
 Serial pc(SERIAL_TX, SERIAL_RX);
 
+DigitalOut fake_switch(D7);
+BusOut rgbleds(LED1, LED2, LED3);
+Ticker pulseblock;
+
+void five_press() {
+  int i = 0;
+  for (i = 0; i < 5; i++) {
+    fake_switch = 0;
+    wait_ms(100);
+    fake_switch = 1;
+    wait_ms(100);
+  }
+}
+
 int main() {
-  uint32_t count = 0;
-  while (1) {
-    pc.printf("im lonely %d\n", count);
-    count++;
-    wait(1.0);
+  fake_switch = 1; // active low switch
+  pulseblock.attach(&five_press, 2.0);
+
+  while(1) {
+    int i = 0;
+    for (i = 0; i < 8;i++) {
+      rgbleds = i;
+      wait(5.0);
+    }
   }
 }
